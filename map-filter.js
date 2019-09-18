996bd4fc7b6bcd1817c4548ee84032b20ca1cbca
mnorris1215 <mnorris1215@yahoo.com>
2019-09-18 17:58:19 -0500

    added addresses on hover

--- map-filter.js
+++ map-filter.js
@@ -1,38 +1,45 @@
-new Vue({
-    el: '#app',
+var coordinates;
+var map;
+function showMap() {
+    map = new google.maps.Map(document.getElementById('gmap'), {
+          zoom: 13,
+          center: new google.maps.LatLng(41.8857718, -87.6656355),
+          mapTypeId: 'terrain'
+        });
+
+    var script = document.createElement("script");
+    script.src = "data.js";
+    document.getElementsByTagName("head")[0].appendChild(script);
+}
 
 
-    computed: {
+window.geojson_callback = function(results) {
+        for (var i = 0; i < results.features.length; i++) {
+        coordinates = results.features[i].geometry.coordinates;
 
-        initMap: {
-            function() {
-              let map;
-              map = new google.maps.Map(document.getElementById('app'), {
-                zoom: 2,
-                center: new google.maps.LatLng(2.8,-187.3),
-                mapTypeId: 'terrain'
-              });
+        var centerCoord = new google.maps.LatLng(coordinates[0], coordinates[1]);
 
-              // Create a <script> tag and set the USGS URL as the source.
-              var script = document.createElement('script');
-              // This example uses a local copy of the GeoJSON stored at
-              // http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_week.geojsonp
-              script.src = 'https://developers.google.com/maps/documentation/javascript/examples/json/earthquake_GeoJSONP.js';
-              document.getElementsByTagName('head')[0].appendChild(script);
-          };
+        var marker = new google.maps.Marker({
+            position: centerCoord,
+            map: map
+          });
 
-            // Loop through the results array and place a marker for each
-            // set of coordinates.
-            window.eqfeed_callback = function(results) {
-              for (var i = 0; i < results.features.length; i++) {
-                var coords = results.features[i].geometry.coordinates;
-                var latLng = new google.maps.LatLng(coords[1],coords[0]);
-                var marker = new google.maps.Marker({
-                  position: latLng,
-                  map: app
-                });
-              }
-            }
-        };
-    };
-});
+          var address = results.features[i].properties.Full_Address;
+
+          var infowindow = new google.maps.InfoWindow({
+              content: address
+            });
+
+    (function(infowindow) {
+
+        google.maps.event.addListener(marker, 'mouseover', function() {
+            infowindow.open(map, this);
+        });
+
+        google.maps.event.addListener(marker, 'mouseout', function() {
+            infowindow.close();
+        });
+
+        })(infowindow);
+    }
+}
