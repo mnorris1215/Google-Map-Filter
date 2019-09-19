9915c5611437ccf254cdefc60e096e400d2906b7
mnorris1215 <mnorris1215@yahoo.com>
2019-09-18 20:19:52 -0500

    added heatmap

--- map-filter.js
+++ map-filter.js
@@ -1,8 +1,10 @@
-var coordinates;
-var map;
+var coordinates, pointArray, map;
+var heatMapData = [];
+
+// Initialize map view
 function showMap() {
     map = new google.maps.Map(document.getElementById('gmap'), {
-          zoom: 13,
+          zoom: 12,
           center: new google.maps.LatLng(41.8857718, -87.6656355),
           mapTypeId: 'terrain'
         });
@@ -12,19 +14,23 @@ function showMap() {
     document.getElementsByTagName("head")[0].appendChild(script);
 }
 
-
+// Iterate through data to create markers for each address
 window.geojson_callback = function(results) {
         for (var i = 0; i < results.features.length; i++) {
         coordinates = results.features[i].geometry.coordinates;
 
-        var centerCoord = new google.maps.LatLng(coordinates[0], coordinates[1]);
 
+        var centerCoord = new google.maps.LatLng(coordinates[0], coordinates[1]);
+        heatMapData.push(centerCoord);
         var marker = new google.maps.Marker({
             position: centerCoord,
             map: map
           });
 
+
+// Info window that displays full address on hover
           var address = results.features[i].properties.Full_Address;
+          var weight = results.features[i].properties.ESTIMATED_MARKET_VALUE
 
           var infowindow = new google.maps.InfoWindow({
               content: address
@@ -42,4 +48,23 @@ window.geojson_callback = function(results) {
 
         })(infowindow);
     }
+
+
+    // Color gradient for estimaed market value
+    //build the array
+
+
+        console.log(heatMapData);
+
+         var pointArray = new google.maps.MVCArray(heatMapData);
+
+        var heatmap = new google.maps.visualization.HeatmapLayer({
+          data: pointArray,
+          radius: 20,
+          map: map
+        });
+
+        heatmap.setMap(map);
+
+
 }
--- testing.js
+++ testing.js
@@ -1,25 +1,31 @@
-var coordinates;
-var map;
-function initialize() {
-    map = new google.maps.Map(document.getElementById('map-canvas'), {
-          zoom: 10,
-          center: new google.maps.LatLng(41.869053399999899,-87.665418399999893),
-          mapTypeId: 'terrain'
-        });
+/* Data points defined as a mixture of WeightedLocation and LatLng objects */
+var heatMapData = [
+  {location: new google.maps.LatLng(37.782, -122.447), weight: 0.5},
+  new google.maps.LatLng(37.782, -122.445),
+  {location: new google.maps.LatLng(37.782, -122.443), weight: 2},
+  {location: new google.maps.LatLng(37.782, -122.441), weight: 3},
+  {location: new google.maps.LatLng(37.782, -122.439), weight: 2},
+  new google.maps.LatLng(37.782, -122.437),
+  {location: new google.maps.LatLng(37.782, -122.435), weight: 0.5},
 
-    var script = document.createElement("script");
-    script.src = "data.js";
-    document.getElementsByTagName("head")[0].appendChild(script);
-}
+  {location: new google.maps.LatLng(37.785, -122.447), weight: 3},
+  {location: new google.maps.LatLng(37.785, -122.445), weight: 2},
+  new google.maps.LatLng(37.785, -122.443),
+  {location: new google.maps.LatLng(37.785, -122.441), weight: 0.5},
+  new google.maps.LatLng(37.785, -122.439),
+  {location: new google.maps.LatLng(37.785, -122.437), weight: 2},
+  {location: new google.maps.LatLng(37.785, -122.435), weight: 3}
+];
 
-window.geojson_callback = function(results) {
-    for (var i = 0; i < results.features.length; i++) {
-        console.log(results.features[i].geometry.coordinates);
-        coordinates = results.features[i].geometry.coordinates;
-        var latLng = new google.maps.LatLng(coordinates[1],coordinates[0]);
-        var marker = new google.maps.Marker({
-            position: latLng,
-            map: map
-          });
-    }
-}
+var sanFrancisco = new google.maps.LatLng(37.774546, -122.433523);
+
+map = new google.maps.Map(document.getElementById('map'), {
+  center: sanFrancisco,
+  zoom: 13,
+  mapTypeId: 'terrain'
+});
+
+var heatmap = new google.maps.visualization.HeatmapLayer({
+  data: heatMapData
+});
+heatmap.setMap(map);
